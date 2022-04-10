4df476ad55a5021ec198eb70ab4f9d3d0ed5c655
yourrra <101475845+yourrra@users.noreply.github.com>
2022-04-10 15:53:11 +0300

    Update get_2.dart

--- bin/get_2.dart
+++ bin/get_2.dart
@@ -1,6 +1,6 @@
 import 'package:dio/dio.dart';
 
 void getHttp() async {
-  var response = await Dio().post('https://porfira-app.ru');
+  var response = await Dio().get('https://porfira-app.ru');
   print(response);
 }
