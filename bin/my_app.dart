b72fa10464750fe41c5fad4e8cbfc6d56d47e678
Артём Полторацкий <81384218+LimitR@users.noreply.github.com>
2022-04-10 15:55:31 +0300

    Update my_app.dart
    Review

--- bin/my_app.dart
+++ bin/my_app.dart
@@ -2,7 +2,5 @@ import 'package:http/http.dart' as http;
 
 void main(List<String> arguments) async {
   var url = Uri.parse('https://porfira-app.ru');
-  var response = await http.get(url);
-  print('Response status: ${response.statusCode}');
-  print('Response body: ${response.body}');
+  var response = await http.post(url).body(arguments);
 }
